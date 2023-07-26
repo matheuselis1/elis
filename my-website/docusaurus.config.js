41e6611de3aa66bf2c6f45d343ade24f34455fe3
matheuselis1 <matheus.bpimenta@senacsp.edu.br>
2023-07-26 08:10:24 -0300

    a

---
+++ my-website/docusaurus.config.js
@@ -0,0 +1,72 @@
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: ' ',
+  tagline: ' ',
+  favicon: 'img/favicon.ico',
+
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+
+  organizationName: 'facebook',
+  projectName: 'docusaurus',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'My Site',
+        items: [
+          {
+           label: 'Docs',
+           to: '/docs/intro',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
